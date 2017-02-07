a7af49f60b436b4dcdfdcd0f9be6a0327b7416f7
Jimmy Van Veen <jimmy.van.veen@gmail.com>
2017-02-07 17:57:48 -0500

    Contact page now works for sending messages to me

--- routes/contact.js
+++ routes/contact.js
@@ -11,7 +11,7 @@ var transporter = nodemailer.createTransport(
   service: 'gmail',
   auth: {
     user: 'jimmy.van.veen@gmail.com',
-    pass: 'yourpass'
+    pass: 'ovuyhibwbrtddcmd'
   }
 });
 
@@ -23,16 +23,6 @@ var recaptcha = new recaptcha2(
   secretKey:'6Ld-sRQUAAAAACRCCHhz1jBnMNNstuFGL_CmPGEp'
 });
 
-/* USE this template to make mail
-var mailOptions = 
-{
-  from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
-  to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
-  subject: 'Hello ✔', // Subject line
-  text: 'Hello world ?', // plain text body
-  html: '<b>Hello world ?</b>' // html body
-};
-*/
 
 // INDEX ROUTE
 router.get("/", function(req, res)
@@ -40,24 +30,48 @@ router.get("/", function(req, res)
   res.render('contact/index');
 });
 
-// req.flash("error", "LOGGED YOU OUT!");
-// req.flash("success", "LOGGED YOU OUT!");
 
 // CREATE ROUTE
 router.post("/", function(req, res)
 {
+  // Make a contact object to send back to the contact page
+  var contact = {title: middleware.sanitize(req.body.title), content: middleware.sanitize(req.body.content) };
+  
   // Validate recaptcha
   recaptcha.validateRequest(req)
   .then(function()
   {
-    // validated and secure
-    req.flash("success", "Message sent");
+    // Process the message, and send it
+    var message =
+    {
+      from: '"JimmyVanVeen.com" <jimmy@jimmyvanveen.com>',  // sender address
+      to: 'jimmy.van.veen@gmail.com',                       // list of receivers
+      subject: 'WEB PAGE CONTACT: ' + contact.title,         // Subject line
+      text: contact.content                                 // plain text body
+    };
+    
+    
+    transporter.sendMail(message, (error, info) =>
+    {
+      if (error)
+      {
+        console.log('Error occurred');
+        req.flash("error", error.message);
+        console.log(error.message);
+        return;
+      }
+      var message = 'Message sent successfully!';
+      
+      console.log(message);
+      req.flash("success", message);
+      console.log('Server responded with "%s"', info.response);
+      transporter.close();
+    });
+    
     res.redirect('/');
   })
   .catch(function(errorCodes)
   {
-    // Make a contact object to send back to the contact page
-    var contact = {title: middleware.sanitize(req.body.title), content: middleware.sanitize(req.body.content) }
     // invalid
     req.flash("error", "Captcha not verified successfully");// translate error codes to human readable text
     res.render('contact/index', {contact: contact});
