ac0e61af3c85b60d8aecf6ee568a91be17e7455f
Jimmy Van Veen <jimmy.van.veen@gmail.com>
2017-11-08 14:47:09 -0500

    Fresh start

--- public/scripts/richTextarea.js
+++
@@ -1,7 +0,0 @@
-// Add the CKEditor to the page
-CKEDITOR.replace('editContent',
-{
-  customConfig: '/scripts/libraries/ckeditor/config.js',
-  filebrowserImageBrowseUrl: '/images',
-  filebrowserUploadUrl: '/images/upload'
-});
