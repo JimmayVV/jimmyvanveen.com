1044f5627d072f5b2e6702c1a9365a32e977e7fc
Jimmy Van Veen <jimmy.van.veen@gmail.com>
2020-09-08 20:58:57 -0400

    Update site icon

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteTitle: "Jimmy Van Veen", // <title>
   description: "Jimmy Van Veen's personal site",
   siteUrl: "https://www.jimmyvanveen.com",
-  manifestName: "SolidState",
-  manifestShortName: "Landing", // max 12 characters
+  manifestName: "JimmyVanVeen.com",
+  manifestShortName: "HomePage", // max 12 characters
   manifestStartUrl: "/",
   manifestBackgroundColor: "#663399",
   manifestThemeColor: "#663399",
   manifestDisplay: "standalone",
-  manifestIcon: "src/assets/img/website-icon.png",
+  manifestIcon: "src/assets/images/jimmy_car.png",
   pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   heading: "Jimmy Van Veen",
   subHeading: "Full time Web Developer. Part time speed demon.",
