8262a4b65f9a234188a0d6400231124a94a6f945
Jimmy Van Veen <jimmy.van.veen@iracing.com>
2023-01-14 22:19:23 -0500

    Add ReCAPTCHA end-to-end

--- app/routes/email.tsx
+++ app/routes/email.tsx
@@ -16,6 +16,30 @@ export async function action({ request }: ActionArgs) {
   const email = formData.get("email") as string
   const phone = formData.get("phone") as string
   const message = formData.get("message") as string
+  const captcha = formData.get("captcha")
+
+  // Ping the google recaptcha verify API to verify the captcha code you received
+  const response = await fetch(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${captcha}`,
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+      },
+      method: "POST",
+    },
+  )
+
+  const data = await response.json()
+
+  /**
+   * The structure of response from the veirfy API is
+   * {
+   *  "success": true|false,
+   *  "challenge_ts": timestamp,  // timestamp of the challenge load (ISO format yyyy-MM-dd'T'HH:mm:ssZZ)
+   *  "hostname": string,         // the hostname of the site where the reCAPTCHA was solved
+   *  "error-codes": [...]        // optional
+    }
+   */
 
   const fields = { name, email, phone, message }
 
@@ -26,8 +50,15 @@ export async function action({ request }: ActionArgs) {
     message: !message ? "Message is required" : null,
   }
 
-  if (Object.values(fieldErrors).some(e => e)) {
-    return json({ success: false, fieldErrors, fields })
+  if (Object.values(fieldErrors).some(e => e) || !data.success) {
+    return json({
+      success: false,
+      fieldErrors,
+      fields,
+      error: !data.success
+        ? "Unproccesable request, Invalid captcha code"
+        : undefined,
+    })
   }
 
   await sendMail({
